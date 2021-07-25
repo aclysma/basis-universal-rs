c90511b8897b86ace72a7da5e8e96206aeb25c52
Philip Degarmo <aclysma@gmail.com>
2021-07-25 15:31:15 -0700

    Fix compile warning when building tests

--- basis-universal-sys/src/lib.rs
+++ basis-universal-sys/src/lib.rs
@@ -1,6 +1,7 @@
 #[allow(non_upper_case_globals)]
 #[allow(non_camel_case_types)]
 #[allow(non_snake_case)]
+#[allow(deref_nullptr)]
 #[rustfmt::skip]
 pub mod transcoding_bindings;
 pub use transcoding_bindings::*;
@@ -8,6 +9,7 @@ pub use transcoding_bindings::*;
 #[allow(non_upper_case_globals)]
 #[allow(non_camel_case_types)]
 #[allow(non_snake_case)]
+#[allow(deref_nullptr)]
 #[rustfmt::skip]
 pub mod encoding_bindings;
 pub use encoding_bindings::*;
